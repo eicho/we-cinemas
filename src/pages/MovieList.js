0c0cafdba04a6d8b895980591878c7992abaa174
Ei Cho <32412506+eicho@users.noreply.github.com>
2023-03-09 11:15:18 +0630

    create custom Hook useFetch

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,20 +1,8 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import { useFetch } from "../hooks/useFetch";
 import { Card } from "../components";
 
-export const MovieList = () => {
-  const [movies, setMovies] = useState([]);
-
-  useEffect(() => {
-    async function fetchMovies() {
-      const response = await fetch(
-        "https://api.themoviedb.org/3/movie/now_playing?api_key=b80d59c33d6d57ed9c7e3713f91c188a"
-      );
-      const data = await response.json();
-      setMovies(data.results);
-    }
-    fetchMovies();
-  }, []);
+export const MovieList = ({ apiPath }) => {
+  const { data: movies } = useFetch(apiPath);
 
   return (
     <main>
